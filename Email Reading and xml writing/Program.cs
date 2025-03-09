4e6a9fb360c4fd1655de281022a299c2c8afd5d1
GedSelenis <selenisgediminas5@gmail.com>
2025-03-09 21:51:54 +0200

    Add project files.

---
+++ Email Reading and xml writing/Program.cs
@@ -0,0 +1,15 @@
+﻿using Email_Reading_and_xml_writing;
+
+
+var mails = OutlookEmails.ReadMailItems();
+int i = 1;
+foreach (var mail in mails)
+{
+    Console.WriteLine("Mail No " + i);
+    Console.WriteLine("Mail Recieved from " + mail.EmailFrom);
+    Console.WriteLine("Mail Subject " + mail.EmailSubject);
+    Console.WriteLine("Mail Body " + mail.EmailBody);
+    Console.WriteLine("");
+    i++;
+}
+Console.ReadKey();
